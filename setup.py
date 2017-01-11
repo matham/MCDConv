52c5de3f39b097272f8f728afa949a3a84e70787
matham <moiein2000@gmail.com>
2017-01-10 20:43:01 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='mcdconv',
+      version='1.0',
+      description='Converts MCD raw binary files to NeoIO nix h5 files.',
+      author='Matthew Einhorn',
+      author_email='moiein2000@gmail.com',
+      license='MIT',
+      packages=['mcdconv'],
+     )
